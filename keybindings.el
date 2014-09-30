0dadc88ee94e85ef8fea07bcd610de75082871dd
paulbdavis <paul@dangersalad.com>
2014-09-30 17:08:12 -0400

    added keybindings for evil nerd commenter

--- keybindings.el
+++ keybindings.el
@@ -6,8 +6,14 @@
 ;;; Code:
 
 (global-set-key (kbd "C-x g") 'magit-status)
+
 (define-key global-map (kbd "C-c r") 'vr/replace)
+
 (global-set-key (kbd "<f9>") 'mu4e)
 
+(global-set-key (kbd "C-c C-/ C-/") 'evilnc-comment-or-uncomment-lines)
+(global-set-key (kbd "C-c C-/ C-l") 'evilnc-comment-or-uncomment-to-the-line)
+(global-set-key (kbd "C-c C-/ C-c") 'evilnc-copy-and-comment-lines)
+(global-set-key (kbd "C-c C-/ C-p") 'evilnc-comment-or-uncomment-paragraphs)
 
 ;;; keybindings.el ends here
