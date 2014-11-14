6b21f3d014cca3406725ea68847ba08b75b4e55a
paulbdavis <paul@dangersalad.com>
2014-11-13 19:12:06 -0500

    auto complete n demand

--- lib/package-loader.el
+++ lib/package-loader.el
@@ -5,7 +5,7 @@
 ;;; Code:
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
--- package-config/auto-complete-config.el
+++ package-config/auto-complete-config.el
@@ -8,6 +8,7 @@
 (require 'auto-complete)
 (add-to-list 'ac-modes 'javascript-mode)
 (global-auto-complete-mode t)
-
+(setq ac-auto-start nil)
+(define-key ac-mode-map (kbd "M-/") 'auto-complete)
 
 ;;; auto-complete-config.el ends here
