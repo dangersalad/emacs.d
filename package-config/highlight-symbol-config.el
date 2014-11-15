32f0db49f0a173354a5cd526484b6c4c9063af2b
paulbdavis <paul@dangersald.com>
2014-11-15 17:49:44 -0500

    no highlight symbol mode for org mode (it's annoying there)

--- package-config/highlight-symbol-config.el
+++ package-config/highlight-symbol-config.el
@@ -9,7 +9,6 @@
 (highlight-symbol-nav-mode)
 
 (add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
-(add-hook 'org-mode-hook (lambda () (highlight-symbol-mode)))
 
 (setq highlight-symbol-idle-delay 0.2
       highlight-symbol-on-navigation-p t)
