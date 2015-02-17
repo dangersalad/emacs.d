cab22f0a5b21bad54f17c000e788174af72a2ce4
paulbdavis <paul@dangersald.com>
2015-02-16 22:02:59 -0500

    use evil for most things again

--- package-config/evil-config.el
+++ package-config/evil-config.el
@@ -10,4 +10,10 @@
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 
 
+(evil-set-initial-state 'prog-mode 'normal)
+(evil-set-initial-state 'git-commit-mode 'insert)
+(evil-set-initial-state 'emacs-lisp-mode 'emacs)
+(evil-set-initial-state 'fundamental-mode 'emacs)
+(evil-mode)
+
 ;;; evil-config.el ends here
