46df98a0709397b16e02d68f38f5c7f5d2a61f46
paulbdavis <paul@dangersald.com>
2015-02-16 22:20:26 -0500

    revert evil changes

--- package-config/evil-config.el
+++ package-config/evil-config.el
@@ -6,19 +6,7 @@
 ;;; Code:
 
 (defvar evil-want-C-u-scroll t)
-
-;; still use4 emacs bindings by default
-(defvar evil-default-state 'emacs)
-
 (require 'evil)
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-
-;; use vim bindings for programming modes
-(evil-set-initial-state 'prog-mode 'normal)
-
-;; we need to specify this since elip is a sub mode of prog-mode
-(evil-set-initial-state 'emacs-lisp-mode 'emacs)
-
-(evil-mode)
 
 ;;; evil-config.el ends here
