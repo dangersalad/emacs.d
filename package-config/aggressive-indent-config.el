e50cce0e9298942a84f98b409429f5e28df862db
paulbdavis <paul@dangersalad.com>
2015-03-08 16:43:25 -0400

    disable aggressive indent

--- package-config/aggressive-indent-config.el
+++ package-config/aggressive-indent-config.el
@@ -5,8 +5,5 @@
 
 ;;; Code:
 (require 'aggressive-indent)
-(add-hook 'prog-mode-hook (lambda ()
-                            (unless (equal major-mode "makefile-gmake-mode")
-                              (aggressive-indent-mode))))
 
 ;;; aggressive-indent-config.el ends here
