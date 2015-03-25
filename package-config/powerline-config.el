a62e3036bb0527a49c1e78293b053c1c8894f526
paulbdavis <paul@dangersald.com>
2015-03-25 13:57:50 -0400

    customize powerline colors

--- package-config/powerline-config.el
+++ package-config/powerline-config.el
@@ -5,7 +5,22 @@
 
 ;;; Code:
 
+(defface powerline-active1 '((t (:background "grey22" :inherit mode-line)))
+  "Powerline face 1."
+  :group 'powerline)
+(defface powerline-active2 '((t (:background "grey27" :inherit mode-line)))
+  "Powerline face 2."
+  :group 'powerline)
+(defface powerline-inactive1
+  '((t (:background "grey25" :foreground "grey11" :italic :inherit mode-line-inactive)))
+  "Powerline face 1."
+  :group 'powerline)
+(defface powerline-inactive2
+  '((t (:background "grey25" :foreground "grey11" :italic :inherit mode-line-inactive)))
+  "Powerline face 2."
+  :group 'powerline)
+
 (require 'powerline)
 (setq powerline-default-separator 'arrow-fade)
-
+(powerline-default-theme)
 ;;; powerline-config.el ends here
