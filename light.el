ff45f1601d46646567c31192af52e4a6d47ba8f6
paulbdavis <paul@dangersald.com>
2015-03-31 23:53:16 -0400

    adaptive wrap and light conf

--- light.el
+++ light.el
@@ -42,50 +42,10 @@ Kept here for easier viewing rather than each package's config.")
 ;; my load path
 (add-to-list 'load-path my-custom-lib)
 
-(defvar my-packages '(ag
-                      adaptive-wrap
-                      flycheck
-                      linum-relative
-                      auto-complete
-                      magit
+(defvar my-packages '(linum-relative
                       undo-tree
-                      git-gutter-fringe
-                      git-timemachine
-                      js2-mode
-                      ac-js2
-                      js2-refactor
-                      restclient
-                      dockerfile-mode
-                      go-mode
-                      go-autocomplete
-                      less-css-mode
                       helm
-                      helm-projectile
-                      helm-git-files
-                      helm-ag
-                      helm-flycheck
-                      helm-descbinds
-                      markdown-mode
-                      yaml-mode
-                      org
-                      org-journal
-                      web-mode
-                      ibuffer-vc
-                      evil
-                      evil-surround
-                      evil-leader
-                      evil-nerd-commenter
-                      key-chord
-                      visual-regexp
-                      projectile
-                      ;; powerline
-                      purpose
-                      aggressive-indent
-                      sunrise-commander
-                      sunrise-x-popviewer
-                      sunrise-x-tree
-                      smartparens
-                      yasnippet)
+                      org)
   "A list of packages to install.")
 
 ;; my cusomized zenburn with darker background
@@ -95,18 +55,14 @@ Kept here for easier viewing rather than each package's config.")
 
 ;; Packages in lib/
 ;; These can all be compiled
-(require 'auto-byte-compile)      ; auto compile lisp, only if a .elc file exists
 (require 'custom-functions)       ; misc custom functions
 (require 'set-local-variable)     ; allows setting a local variable easily
 (require 'indentation)            ; indentation rules
 (require 'line-opening)           ; vim like line opening C-o and M-o
 (require 'marks)                  ; mark tweaks
-(require 'popup-terminal)         ; terminal set to <F11>
 (require 'diminish)               ; hide minor modes in modeline
-;; (require 'local-powerline-themes) ; hide minor modes in modeline
 (require 'package-loader)         ; auto load packages from MELPA
 (require 'erc-settings)           ; settings for erc
-(require 'unit-file-mode)         ; a major mode for editing systemd unit files
 
 (setq newline-and-indent t)   ; enable indentation detection for line-opening
 
@@ -154,33 +110,17 @@ Kept here for easier viewing rather than each package's config.")
  kept-old-versions 2
  version-control t)
 
-;; move windows with Shift + <arrow>
-(windmove-default-keybindings)
-(setq windmove-wrap-around t)
-
 ;; no menu bar please
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; calendar shortcut
-(global-set-key (kbd "<f3>") 'calendar)
-
 ;; font for gui
 (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-8"))
-
-
-;; winner mode
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
 
 ;; unique buffer names
 (require 'uniquify)
 (customize-set-variable 'uniquify-buffer-name-style 'post-forward)
-
-;; ibuffer
-(defalias 'list-buffers 'ibuffer)
-(setq ibuffer-use-other-window t)
 
 ;; detect changes on disk and update buffers
 (global-auto-revert-mode)
@@ -203,18 +143,12 @@ Kept here for easier viewing rather than each package's config.")
 (message "Loading package configurations")
 (my-package-init)
 
-;; load keybindings
-(load my-keybindings)
-
 ;; flat mode line
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 
 ;; load macros
 (load my-macros)
-
-;; load email configuration
-(load "~/.emacs.d/email/email-config")
 
 ;; dired
 (setq dired-listing-switches "-lha --group-directories-first")
@@ -223,5 +157,6 @@ Kept here for easier viewing rather than each package's config.")
 (if (file-readable-p my-local-init)
     (load (concat my-local-init)))
 
-(provide 'init)
-;;; init.el ends here
+(provide 'my-light-config)
+
+;;; light.el ends here
---
+++ package-config/adaptive-wrap-config.el
@@ -0,0 +1,11 @@
+;;; adaptive-wrap-config --- Config for ELPA installed packaged
+;;; Commentary:
+;;;
+;;; Config for adaptive-wrap package
+
+;;; Code:
+
+(defvar adaptive-wrap-extra-indent 2)
+(require 'adaptive-wrap)
+
+;;; adaptive-wrap-config.el ends here
