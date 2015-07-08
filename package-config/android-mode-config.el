63956ffc57b08fca378c8211a6719a5bdc470a54
paulbdavis <paul@dangersalad.com>
2015-07-07 23:04:43 -0400

    android mode

---
+++ package-config/android-mode-config.el
@@ -0,0 +1,12 @@
+;;; android-mode-config --- Config for ELPA installed packaged
+;;; Commentary:
+;;;
+;;; Config for android-mode package
+
+;;; Code:
+
+(require 'android-mode)
+(custom-set-variables '(android-mode-sdk-dir "/opt/android-sdk"))
+
+
+;;; android-mode-config.el ends here
