4bf4066a101f874e25cc1b8ef96b6b12be3cfdfa
paulbdavis <paul@dangersalad.com>
2015-09-18 00:22:13 -0400

    ledger custom reports

--- custom.el
+++ custom.el
@@ -3,6 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   (quote
+    (("asset/liabilities" "ledger -f %(ledger-file) bal assets liabilities")
+     ("profit/loss" "ledger -f %(ledger-file) bal income expenses")
+     ("checkbook" "ledger -f %(ledger-file) reg personal:assets:checking")
+     ("cc" "ledger -f %(ledger-file) reg personal:liabilities and visa")
+     ("loans" "ledger -f %(ledger-file) reg personal:liabilities and loan personal:expense and loan")
+     ("bal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)"))))
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
