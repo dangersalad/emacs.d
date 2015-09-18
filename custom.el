7e5ec362213559dc060439940b42a8965eeaf51e
paulbdavis <paul@dangersalad.com>
2015-09-17 22:09:27 -0400

    magit default

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-merge-arguments (quote ("--no-ff")))
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
