15fbe732b9daf824352f14cdb1b762b69d6ac893
paulbdavis <paul@dangersalad.com>
2015-09-25 02:03:28 -0400

    fix some faces

--- package-config/zenburn-theme-config.el
+++ package-config/zenburn-theme-config.el
@@ -20,16 +20,16 @@
   (set-face-attribute 'powerline-inactive1 nil :background zenburn-bg+05 :foreground zenburn-bg+3)
   (set-face-attribute 'powerline-inactive2 nil :background zenburn-bg+05 :foreground zenburn-bg+3)
   (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
-  (set-face-attribute 'flycheck-error nil :underline '(:style line))
-  (set-face-attribute 'flycheck-warning nil :underline '(:style line))
-  (set-face-attribute 'flycheck-info nil :underline '(:style line))
+  (set-face-attribute 'flycheck-error nil :underline `(:style line :color ,zenburn-red-1))
+  (set-face-attribute 'flycheck-warning nil :underline `(:style line :color ,zenburn-yellow-2))
+  (set-face-attribute 'flycheck-info nil :underline `(:style line :color ,zenburn-blue-2))
   (set-face-attribute 'helm-source-header nil :background zenburn-bg+2 :height 1.3 :box '(:style nil))
   (set-face-attribute 'helm-selection nil :background zenburn-bg+2 :weight 'bold)
   (set-face-attribute 'helm-selection-line nil :background zenburn-bg+2)
 
-  (set-face-attribute 'flycheck-color-mode-line-error-face nil :background zenburn-red-2 :foreground zenburn-bg-1)
-  (set-face-attribute 'flycheck-color-mode-line-warning-face nil :background zenburn-yellow-4 :foreground zenburn-bg-1)
-  (set-face-attribute 'flycheck-color-mode-line-info-face nil :background zenburn-blue-2 :foreground zenburn-bg-1)
+  ;; (set-face-attribute 'flycheck-color-mode-line-error-face nil :background zenburn-red-2 :foreground zenburn-bg-1)
+  ;; (set-face-attribute 'flycheck-color-mode-line-warning-face nil :background zenburn-yellow-4 :foreground zenburn-bg-1)
+  ;; (set-face-attribute 'flycheck-color-mode-line-info-face nil :background zenburn-blue-2 :foreground zenburn-bg-1)
 
   )
 
