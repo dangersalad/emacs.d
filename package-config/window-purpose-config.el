24ff493fc32c46e72972aaf61573a623154b410e
paulbdavis <paul@dangersalad.com>
2016-01-21 20:11:57 -0500

    changes to putrpose mode

--- package-config/window-purpose-config.el
+++ package-config/window-purpose-config.el
@@ -6,18 +6,42 @@
 ;;; Code:
 
 (require 'window-purpose)
+(require 'window-purpose-prefix-overload)
 
 (add-to-list 'purpose-user-mode-purposes '(js-mode . code))
 (add-to-list 'purpose-user-mode-purposes '(web-mode . code))
 (add-to-list 'purpose-user-mode-purposes '(prog-mode . code))
 (add-to-list 'purpose-user-mode-purposes '(term-mode . aux))
+(add-to-list 'purpose-user-mode-purposes '(eshell-mode . aux))
 (add-to-list 'purpose-user-mode-purposes '(dired-mode . aux))
 (add-to-list 'purpose-user-mode-purposes '(git-commit-mode . popup))
 (add-to-list 'purpose-user-regexp-purposes '("magit" . popup))
-(add-to-list 'purpose-user-regexp-purposes '("helm" . popup))
 (add-to-list 'purpose-user-name-purposes '("*Help*" . popup))
 (add-to-list 'purpose-user-name-purposes '("*scratch*" . aux))
 
+(defalias 'purpose-friendly-find-file
+  (purpose-ido-caller #'ido-find-file #'helm-find-files))
+
+(defalias 'find-file-without-purpose
+  (without-purpose-command #'helm-find-files))
+
+(define-purpose-prefix-overload purpose-find-file-overload
+  '(purpose-friendly-find-file find-file-without-purpose))
+
+(defalias 'purpose-friendly-switch-buffer
+  (purpose-ido-caller #'ido-find-file #'helm-mini))
+
+(defalias 'switch-buffer-without-purpose
+  (without-purpose-command #'helm-mini))
+
+(define-purpose-prefix-overload purpose-find-file-overload
+  '(purpose-friendly-find-file find-file-without-purpose))
+
+(define-purpose-prefix-overload purpose-switch-buffer-overload
+  '(purpose-friendly-switch-buffer switch-buffer-without-purpose))
+
+(define-key purpose-mode-map (kbd "C-x C-f") 'purpose-find-file-overload)
+(define-key purpose-mode-map (kbd "C-x b") 'purpose-switch-buffer-overload)
 
 (purpose-compile-user-configuration)
 
