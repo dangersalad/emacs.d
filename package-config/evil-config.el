614a30693cc1257d824bc205c86757c81b3049a4
paulbdavis <paul@dangersalad.com>
2016-02-12 13:16:39 -0500

    fix evil scroll C-u

--- package-config/evil-config.el
+++ package-config/evil-config.el
@@ -6,8 +6,16 @@
 ;;; Code:
 
 (defvar evil-want-C-u-scroll t)
+
 (require 'evil)
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-insert-state-map (kbd "C-u")
+  (lambda ()
+    (interactive)
+    (evil-delete (point-at-bol) (point))))
 
 ;; toggle vim bindings with C-f1
 (global-set-key (kbd "<f12>") 'evil-local-mode)
